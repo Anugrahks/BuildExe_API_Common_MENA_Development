4c5b8d29a1e10a06a382b9af3ccc1a0d7995f251
Anugrahks <anugrahks075@gmail.com>
2026-04-04 10:01:12 +0530

    latest

--- Basic/Repository/IPrintableReportConfigurationRepository.cs
+++ Basic/Repository/IPrintableReportConfigurationRepository.cs
@@ -44,6 +44,7 @@ namespace BuildExeBasic.Repository
         Task<string> StaticPrintableProformaInvoice(int BranchId, int ReportId, int RecordId);
 
         Task<string> StaticPrintableEstimation(int BranchId, int ReportId, int RecordId, int IsEnquiryWise);
+        Task<string> StaticPrintableInvoiceGeneration(int BranchId, int ReportId, int RecordId);
 
 
 
