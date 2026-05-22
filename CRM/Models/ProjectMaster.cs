272426c426c37584087ac56761886cbea129d9df
Sharika <kvsharikaunnikrishnan@gmail.com>
2026-05-22 16:48:22 +0530

    promiseDate in ProjectMaster

--- CRM/Models/ProjectMaster.cs
+++ CRM/Models/ProjectMaster.cs
@@ -68,6 +68,9 @@ namespace BuildExeServices.Models
         public DateTime DatePeriodFrom { get; set;}
         public DateTime DatePeriodTo { get; set; }
 
+        public DateTime? promiseDate { get; set; }
+        
+
         //public string? OpeningType { get; set; }
         //public decimal? OpeningBalance { get; set; }
 
