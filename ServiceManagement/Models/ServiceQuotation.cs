cf651c400080f92d69a8816f4c53ab012cf23879
SafvanNS <safvan4348@gmail.com>
2026-05-30 15:06:35 +0530

    latestModel

--- ServiceManagement/Models/ServiceQuotation.cs
+++ ServiceManagement/Models/ServiceQuotation.cs
@@ -13,7 +13,9 @@ namespace BuildExeServiceManagement.Models
         public int Id { get; set; }                 // For update
         public int InvoiceNo { get; set; }
         public DateTime InvoiceDate { get; set; }
+        public DateTime BillDate { get; set; }
         public string RefNo { get; set; }
+        public string BillNo { get; set; }
         public string ServiceType { get; set; }
         public int VoucherTypeId { get; set; }
         public int VoucherNumber { get; set; }
@@ -37,6 +39,7 @@ namespace BuildExeServiceManagement.Models
         public int IsDeleted { get; set; }
         public DateTime ApprovedDate { get; set; }
         public int JobNo { get; set; }
+        public int JobId { get; set; }
         public List<ServiceQuotationDetails> ServiceInvoiceDetails { get; set; }
 
     }
@@ -47,6 +50,8 @@ namespace BuildExeServiceManagement.Models
         public int ServiceInvDetailId { get; set; } 
         public int InvoiceId { get; set; }
         public int MaterialId { get; set; }
+        public int Id { get; set; }
+        public int JobNo { get; set; }
         public string PartNo { get; set; }
         public string Description { get; set; }
         public decimal Quantity { get; set; }
