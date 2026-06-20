109f671f6b0681636645869ff4e444316ad4e93a
git4Sreenath <sreenathkelthattil@gmail.com>
2026-06-20 09:36:27 +0530

    updates in MaterialRecieveListController

--- Material/Repository/MaterialReceiveRepository.cs
+++ Material/Repository/MaterialReceiveRepository.cs
@@ -360,5 +360,47 @@ namespace BuildExeMaterialServices.Repository
                 return null;
             }
         }
+        public async Task<string> Getbalancesbyid(int MaterialTransferId)
+        {
+            try
+            {
+                DbCommand cmd = _dbContext.Database.GetDbConnection().CreateCommand();
+
+                cmd.CommandText = "dbo.Stpro_MaterialRecieve";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int) { Value = MaterialTransferId });
+                cmd.Parameters.Add(new SqlParameter("@json", SqlDbType.NVarChar) { Value = "" });
+                cmd.Parameters.Add(new SqlParameter("@CompanyId", SqlDbType.Int) { Value = 0 });
+                cmd.Parameters.Add(new SqlParameter("@BranchId", SqlDbType.Int) { Value = 0 });
+                cmd.Parameters.Add(new SqlParameter("@UserId", SqlDbType.Int) { Value = 0 });
+                cmd.Parameters.Add(new SqlParameter("@Action", SqlDbType.Int) { Value = 10 });
+                if (cmd.Connection.State != ConnectionState.Open)
+                {
+                    cmd.Connection.Open();
+                }
+
+                DbDataReader reader = await cmd.ExecuteReaderAsync();
+
+                var dataTable = new DataTable();
+                dataTable.Load(reader);
+                string purcasedetails = "";
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    purcasedetails = purcasedetails + dataTable.Rows[i][0].ToString();
+                }
+                if (purcasedetails == "")
+                    purcasedetails = "[]";
+                return purcasedetails;
+
+                
+            }
+            catch (Exception ex)
+            {
+                Logger.ErrorLog(this.GetType().Name, MethodBase.GetCurrentMethod().Name, ex);
+
+                return null;
+            }
+        }
     }
 }
