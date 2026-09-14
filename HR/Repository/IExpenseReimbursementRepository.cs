979d580e899113813a1436c7947c8c7e56be02ee
git4Sreenath <sreenathkelthattil@gmail.com>
2026-09-14 12:50:45 +0530

    Created ExpenseReimbursement Controller and repositories

---
+++ HR/Repository/IExpenseReimbursementRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildExeHR.Models;
+namespace BuildExeHR.Repository
+{
+    public interface IExpenseReimbursementRepository
+    {
+
+        Task<IEnumerable<Validation>> Insert(IEnumerable<ExpenseReimbursement> items);
+        Task<IEnumerable<Validation>> Update(IEnumerable<ExpenseReimbursement> items);
+
+        Task<string> GetById(int id, int companyId, int branchId);
+        Task<string> GetAll(int companyId, int branchId, int financialYearId);
+        Task Delete(int id, int userId);
+    }
+}
