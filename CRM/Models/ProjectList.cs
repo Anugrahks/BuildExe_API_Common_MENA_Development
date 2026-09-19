9bf3b279c980703affd1e282fc2223cad873bd4e
DELL <DELL@DESKTOP-EMC43QK>
2026-09-19 16:48:28 +0530

    latest

--- CRM/Models/ProjectList.cs
+++ CRM/Models/ProjectList.cs
@@ -47,7 +47,7 @@ namespace BuildExeServices.Models
         public int IsBlockFloorExists { get; set; }
 
         public string LpoNo { get; set; }
-        public DateTime LpoDate { get; set; }
+        p
 
         //public string ClientName { get; set; }
     }
